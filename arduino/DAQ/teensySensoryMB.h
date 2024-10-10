c566f8039c391a105b8aa27e14403bd669eafe47
Andy <andy7093li@gmail.com>
2024-10-10 15:23:11 -0400

    Updated DAQ

--- arduino/DAQ/teensySensoryMB.h
+++ arduino/DAQ/teensySensoryMB.h
@@ -11,9 +11,9 @@
 
 
 #define CUST_BYTE_NUM_OUT	0
-#define CUST_BYTE_NUM_IN	24
+#define CUST_BYTE_NUM_IN	128
 #define TOT_BYTE_NUM_ROUND_OUT	0
-#define TOT_BYTE_NUM_ROUND_IN	24
+#define TOT_BYTE_NUM_ROUND_IN	128
 
 
 typedef union												//---- output buffer ----
@@ -30,11 +30,39 @@ typedef union												//---- input buffer ----
 	uint8_t  Byte [TOT_BYTE_NUM_ROUND_IN];
 	struct
 	{
-		double      Force1;
-		double      Force2;
-		double      Force3;
+		float       Force1;
+		float       Force2;
+		float       Force3;
+		float       Force4;
+		float       Force5;
+		float       Force6;
+		float       Force7;
+		float       Force8;
+		float       Force9;
+		float       Force10;
+		float       Force11;
+		float       Force12;
+		float       Force13;
+		float       Force14;
+		float       Force15;
+		float       Force16;
+		float       Force17;
+		float       Force18;
+		float       Force19;
+		float       Force20;
+		float       Force21;
+		float       Force22;
+		float       Force23;
+		float       Force24;
+		float       Force25;
+		float       Force26;
+		float       Force27;
+		float       Force28;
+		float       Force29;
+		float       Force30;
+		float       Force31;
+		float       Force32;
 	}Cust;
 } PROCBUFFER_IN;
 
 #endif
-
